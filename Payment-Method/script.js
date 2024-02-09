11a1577ebd8aaded20097150784d97eb53631f79
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-02-09 11:13:24 +0530

    made payment methods user responsive

---
+++ Payment-Method/script.js
@@ -0,0 +1,43 @@
+const myDiv = document.getElementById('myDiv');
+const pay = document.getElementById('pay-btn1');
+// add event listener to pseudo element card-pay:after
+const cardPay = document.querySelector('.card-pay');
+const myDiv2 = document.getElementById('myDiv2');
+const cardPay2 = document.querySelector('.card-pay2');
+//get form
+const form = document.getElementById('form');
+
+
+
+myDiv.addEventListener('click', function() {
+    console.log('You clicked on pay by card option!');
+    // change display of pay-btn
+    if (pay.style.display === 'none') {
+        pay.style.display = 'block';
+    } else {
+        pay.style.display = 'none';
+        //reset form
+        form.reset();
+    }
+    
+
+    if (cardPay.classList.contains('changed')) {
+        cardPay.classList.remove('changed');
+    } else {
+        cardPay.classList.add('changed');
+    }
+}
+);
+myDiv2.addEventListener('click', function() {
+    console.log('You chose to pay later!');
+    // change display of pay-btn
+    
+    
+
+    if (cardPay2.classList.contains('changed')) {
+        cardPay2.classList.remove('changed');
+    } else {
+        cardPay2.classList.add('changed');
+    }
+}
+);
